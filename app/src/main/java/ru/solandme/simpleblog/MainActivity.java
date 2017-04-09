7ca5ed33c3032dff21c0a7bc6b5edad1b03f8cc9
Hyunwoo Song <shw5258@naver.com>
2017-04-10 03:04:56 +0900

    Initial commit

---
+++ app/src/main/java/ru/solandme/simpleblog/MainActivity.java
@@ -0,0 +1,265 @@
+package ru.solandme.simpleblog;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    
+    public static final String POST_KEY = "postKey";
+    private RecyclerView blogList;
+    private DatabaseReference databaseRef;
+    private DatabaseReference databaseRefUsers;
+    private DatabaseReference databaseRefCurrentUser;
+    private DatabaseReference databaseRefLike;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private Query queryCurrentUser;
+    private String user_id;
+
+    private Boolean processLike;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                } else{
+                    user_id = auth.getCurrentUser().getUid();
+                }
+            }
+        };
+
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("Blog");
+        databaseRefUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        databaseRefLike = FirebaseDatabase.getInstance().getReference().child("Likes");
+    
+        if (auth.getCurrentUser() != null) {
+            String currentUserId = auth.getCurrentUser().getUid();
+            databaseRefCurrentUser = FirebaseDatabase.getInstance().getReference().child("Blog");
+            queryCurrentUser = databaseRefCurrentUser.orderByChild("uid").equalTo(currentUserId);
+        }
+    
+        databaseRef.keepSynced(true);
+        databaseRefUsers.keepSynced(true);
+        databaseRefLike.keepSynced(true);
+
+        blogList = (RecyclerView) findViewById(R.id.blogList);
+
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setReverseLayout(true);
+//        layoutManager.setStackFromEnd(true);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+        blogList.setHasFixedSize(true);
+        blogList.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+        checkUserExist();
+        FirebaseRecyclerAdapter<Blog, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
+                        Blog.class,
+                        R.layout.blog_row,
+                        BlogViewHolder.class,
+//                        queryCurrentUser
+                        databaseRef
+                ) {
+                    @Override
+                    protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
+
+                        final String postKey = getRef(position).getKey();
+
+                        viewHolder.setTitle(model.getTitle());
+//                        viewHolder.setDesc(model.getDescription());
+                        viewHolder.setImage(getApplicationContext(), model.getImageURL());
+                        viewHolder.setUsername(model.getUsername());
+                        viewHolder.setLikeBtn(postKey);
+                        viewHolder.view.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent singleBlogIntent = new Intent(MainActivity.this, BlogSingleActivity.class);
+                                singleBlogIntent.putExtra(POST_KEY, postKey);
+                                startActivity(singleBlogIntent);
+                            }
+                        });
+
+                        viewHolder.likeBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                processLike = true;
+                                databaseRefLike.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                        if (processLike) {
+                                            if (dataSnapshot.child(postKey).hasChild(auth.getCurrentUser().getUid())) {
+                                                databaseRefLike.child(postKey).child(auth.getCurrentUser().getUid()).removeValue();
+                                                processLike = false;
+                                            } else {
+                                                databaseRefLike.child(postKey).child(auth.getCurrentUser().getUid()).setValue("RandomValue");
+                                                processLike = false;
+                                            }
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        });
+                    }
+                };
+        blogList.setAdapter(firebaseRecyclerAdapter);
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authStateListener);
+    }
+    
+    public static class BlogViewHolder extends RecyclerView.ViewHolder {
+
+        View view;
+        ImageButton likeBtn;
+        DatabaseReference databaseRefLike;
+        FirebaseAuth auth;
+
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+            likeBtn = (ImageButton) view.findViewById(R.id.likeBtn);
+            databaseRefLike = FirebaseDatabase.getInstance().getReference().child("Likes");
+            auth = FirebaseAuth.getInstance();
+
+            databaseRefLike.keepSynced(true);
+        }
+
+        void setTitle(String title) {
+            TextView postTitle = (TextView) view.findViewById(R.id.postTitle);
+            postTitle.setText(title);
+        }
+
+        void setLikeBtn(final String postKey) {
+            databaseRefLike.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.child(postKey).hasChild(auth.getCurrentUser().getUid())) {
+                        likeBtn.setImageResource(R.drawable.ic_like);
+                    } else {
+                        likeBtn.setImageResource(R.drawable.ic_like_grey);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+//        void setDesc(String desc) {
+//            TextView postDescription = (TextView) view.findViewById(R.id.postDesc);
+//            postDescription.setText(desc);
+//        }
+
+        void setImage(Context context, String imageUrl) {
+            ImageView postImage = (ImageView) view.findViewById(R.id.postImage);
+            postImage.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+            int width = postImage.getMeasuredWidth();
+            int height = postImage.getMeasuredHeight();
+            Picasso.with(context).load(imageUrl).resize(width, height).centerInside().into(postImage);
+//            Glide.with(context).load(imageUrl).into(postImage);
+            Log.d(MainActivity.class.getCanonicalName(), "postImageView Width: " + postImage.getWidth());
+        }
+        //datachange
+
+        void setUsername(String username) {
+            TextView postUsername = (TextView) view.findViewById(R.id.postUsername);
+            postUsername.setText(username);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_add) {
+            startActivity(new Intent(MainActivity.this, PostActivity.class));
+        }
+
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        auth.signOut();
+    }
+
+
+    private void checkUserExist() {
+
+        databaseRefUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null && dataSnapshot.hasChild(user_id)
+                        && (!dataSnapshot.child(user_id).hasChild("name") || !dataSnapshot.child(user_id).hasChild("image"))) {
+                    Intent setupIntent = new Intent(MainActivity.this, SetupActivity.class);
+                    setupIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(setupIntent);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/ru/solandme/simpleblog/PostActivity.java
@@ -0,0 +1,149 @@
+package ru.solandme.simpleblog;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class PostActivity extends AppCompatActivity {
+
+    private ImageButton selectImage;
+    private EditText postTitle;
+    private EditText postDesc;
+    private Button submitBtn;
+    private Uri imageUri = null;
+
+    private StorageReference storage;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    private DatabaseReference databaseRefUser;
+    private int mWidth = 0;
+    private int mHeight = 0;
+
+    private ProgressDialog progress;
+
+    private static final int GALLERY_REQUEST = 1;
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+        storage = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Blog");
+        databaseRefUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser.getUid());
+
+        selectImage = (ImageButton) findViewById(R.id.imageSelect);
+        selectImage.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        mWidth = selectImage.getMeasuredWidth();
+        mHeight = selectImage.getMeasuredHeight();
+        
+        
+        postTitle = (EditText) findViewById(R.id.titleField);
+        postDesc = (EditText) findViewById(R.id.descField);
+        submitBtn = (Button) findViewById(R.id.submitButton);
+
+        progress = new ProgressDialog(this);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        progress.setMessage("Posting to Blog ...");
+
+        final String title_val = postTitle.getText().toString().trim();
+        final String desc_val = postDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && imageUri != null) {
+
+            progress.show();
+
+            StorageReference filePath = storage.child("Blog_Images").child(imageUri.getLastPathSegment());
+
+            filePath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    final Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    final DatabaseReference newPostRef = databaseReference.push();
+
+                    databaseRefUser.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            newPostRef.child("title").setValue(title_val);
+                            newPostRef.child("description").setValue(desc_val);
+                            newPostRef.child("imageURL").setValue(downloadUrl.toString());
+                            newPostRef.child("uid").setValue(currentUser.getUid());
+                            newPostRef.child("username").setValue(dataSnapshot.child("name")
+                                    .getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(PostActivity.this, MainActivity.class));
+                                    }
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    progress.dismiss();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            Picasso.with(getBaseContext()).load(imageUri).resize(mWidth, mHeight).centerInside().into(selectImage);
+        }
+    }
+}
